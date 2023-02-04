69803518627471730fa346f59e89918790331ad4
fknt00 <fknt00@gmail.com>
2023-02-04 17:20:20 -0300

    Update mdc.py

--- Orientação a Objetos/mdc.py
+++ Orientação a Objetos/mdc.py
@@ -1,7 +1,4 @@
 def mdc(a, b):
-    divisores_de_a = [x for x in range(1, a) if (x % 2 == 0)]
-    divisores_de_b = [y for y in range(1, b) if (y % 2 == 0)]
-
     multiplos_de_a = [x for x in range(1, a + 1) if ((a / x) == int(a / x))]
     multiplos_de_b = [y for y in range(1, b + 1) if ((b / y) == int(b / y))]
 
