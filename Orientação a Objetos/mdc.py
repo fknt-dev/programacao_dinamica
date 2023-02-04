b8fe98ed6305b4d7510d71a94cc0f7418698e440
fknt00 <fknt00@gmail.com>
2023-02-04 17:03:00 -0300

    Update mdc.py

--- Orientação a Objetos/mdc.py
+++ Orientação a Objetos/mdc.py
@@ -1,32 +1,37 @@
 def mdc(a, b):
-    divisores_a = [x for x in range(1, a+1) if (x % 2 == 0)]
-    divisores_b = [y for y in range(1, b+1) if (y % 2 == 0)]
+    divisores_de_a = [x for x in range(1, a+1) if (x % 2 == 0)]
+    divisores_de_b = [y for y in range(1, b+1) if (y % 2 == 0)]
 
-    if len(divisores_a) >= len(divisores_b):
-        tamanho = int(len(divisores_b))
-        maior = divisores_a
-        menor = divisores_b
+    if len(divisores_de_a) >= len(divisores_de_b):
+        # tamanho = int(len(divisores_de_b))
+        maior = divisores_de_a
+        menor = divisores_de_b
     else:
-        tamanho = int(len(divisores_a))
-        maior = divisores_b
-        menor = divisores_a
+        # tamanho = int(len(divisores_de_a))
+        maior = divisores_de_b
+        menor = divisores_de_a
+    tamanho = int(len(menor))
 
     divisores = [num for num in menor if num in maior]
 
-    multiplos_a = []
-    multiplos_b = []
+    multiplos_de_a = []
+    multiplos_de_b = []
 
     for i in range(tamanho):
         if a / divisores[i] == int(a / divisores[i]):
-            multiplos_a.append(divisores[i])
+            multiplos_de_a.append(divisores[i])
         if b / divisores[i] == int(b / divisores[i]):
-            multiplos_b.append(divisores[i])
+            multiplos_de_b.append(divisores[i])
 
     novo = []
-    for num in multiplos_a:
-        if num in multiplos_b:
+    for num in multiplos_de_a:
+        if num in multiplos_de_b:
             novo.append(num)
 
     maior_divisor = novo[len(novo) - 1]
 
     return maior_divisor
+
+if __name__ == '__main__':
+    from sys import argv
+    mdc(int(argv[1]), int(argv[2]))
