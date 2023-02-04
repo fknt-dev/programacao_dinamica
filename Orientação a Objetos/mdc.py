031987f19eaa86894dbe320e89b6021728eabff0
fknt00 <fknt00@gmail.com>
2023-02-04 17:18:59 -0300

    Update mdc.py

--- Orientação a Objetos/mdc.py
+++ Orientação a Objetos/mdc.py
@@ -2,19 +2,8 @@ def mdc(a, b):
     divisores_de_a = [x for x in range(1, a) if (x % 2 == 0)]
     divisores_de_b = [y for y in range(1, b) if (y % 2 == 0)]
 
-    if len(divisores_de_a) >= len(divisores_de_b):
-        tamanho = int(len(divisores_de_b))
-    else:
-        tamanho = int(len(divisores_de_a))
-
-    multiplos_de_a = []
-    multiplos_de_b = []
-
-    for num in range(1, tamanho):
-        if a / num == int(a / num):
-            multiplos_de_a.append(num)
-        if b / num == int(b / num):
-            multiplos_de_b.append(num)
+    multiplos_de_a = [x for x in range(1, a + 1) if ((a / x) == int(a / x))]
+    multiplos_de_b = [y for y in range(1, b + 1) if ((b / y) == int(b / y))]
 
     novo = [num for num in multiplos_de_a if num in multiplos_de_b]
 
